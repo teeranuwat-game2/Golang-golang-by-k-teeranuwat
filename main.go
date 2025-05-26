78a92576a6676ca58fe42da612b3a94b8316c451
teeranuwat.u@arise.tech <teeranuwat.u@arise.tech>
2025-05-26 19:45:02 +0700

    add restapi

--- main.go
+++ main.go
@@ -1,40 +1,9 @@
 package main
 
-import "net/http"
-
-type User struct {
-	Name  string
-	Age   int
-	Email string
-}
+import (
+	"dayone/restapi"
+)
 
 func main() {
-
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		username := getUsername()
-		user, err := getUser()
-		if err != nil {
-			http.Error(w, "User not found", http.StatusNotFound)
-			return
-		}
-
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello " + username + "! User: " + user.Name + ", Age: " + string(user.Age)))
-	})
-
-	http.ListenAndServe(":8080", nil)
-	// curl -X GET http://localhost:8080/hello
-}
-
-func getUsername() string {
-	return "Alice"
-}
-
-func getUser() (User, error) {
-	// Simulating a user retrieval
-	return User{
-		Name:  "Bob",
-		Age:   30,
-		Email: "",
-	}, nil
+	restapi.Init()
 }
--- restapi/restapi.go
+++ restapi/restapi.go
@@ -1,5 +1,39 @@
 package restapi
 
-func Init() {
+import "net/http"
 
+type User struct {
+	Name  string
+	Age   int
+	Email string
+}
+
+func Init() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		username := getUsername()
+		user, err := getUser()
+		if err != nil {
+			http.Error(w, "User not found", http.StatusNotFound)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello " + username + "! User: " + user.Name + ", Age: " + string(user.Age)))
+	})
+
+	http.ListenAndServe(":8080", nil)
+	// curl -X GET http://localhost:8080/hello
+}
+
+func getUsername() string {
+	return "Alice"
+}
+
+func getUser() (User, error) {
+	// Simulating a user retrieval
+	return User{
+		Name:  "Bob",
+		Age:   30,
+		Email: "",
+	}, nil
 }
