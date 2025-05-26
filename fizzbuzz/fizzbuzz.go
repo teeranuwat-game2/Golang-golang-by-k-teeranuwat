1314cb28b2c46eb9aa5d6281c6c71572b8ddc843
teeranuwat.u@arise.tech <teeranuwat.u@arise.tech>
2025-05-26 20:00:30 +0700

    add fizzbuzz

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,1 +1,29 @@
 package fizzbuzz
+
+func FizzBuzz(n int) {
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			println("FizzBuzz")
+		} else if i%3 == 0 {
+			println("Fizz")
+		} else if i%5 == 0 {
+			println("Buzz")
+		} else {
+			println(i)
+		}
+	}
+}
+
+func FizzBuzzSlice(numbers []int) {
+	for _, i := range numbers {
+		if i%3 == 0 && i%5 == 0 {
+			println("FizzBuzz")
+		} else if i%3 == 0 {
+			println("Fizz")
+		} else if i%5 == 0 {
+			println("Buzz")
+		} else {
+			println(i)
+		}
+	}
+}
