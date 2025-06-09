a2a8cdd547f1a242a57a4087bdceba814252d206
teeranuwat.u@arise.tech <teeranuwat.u@arise.tech>
2025-06-09 19:41:33 +0700

    add db call from generate

--- gensql/postgresdb/models.go
+++ gensql/postgresdb/models.go
@@ -2,7 +2,7 @@
 // versions:
 //   sqlc v1.29.0
 
-package tutorial
+package postgresdb
 
 import (
 	"github.com/jackc/pgx/v5/pgtype"
--- gensql/postgresdb/query.sql.go
+++ gensql/postgresdb/query.sql.go
@@ -3,7 +3,7 @@
 //   sqlc v1.29.0
 // source: query.sql
 
-package tutorial
+package postgresdb
 
 import (
 	"context"
